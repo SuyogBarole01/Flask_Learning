38180a739ced773c0d84fb9a1070642bc4359038
SuyogBarole01 <suyogbarole02@gmail.com>
2026-01-03 19:06:20 +0530

    Creation of controller model architecture in Flask

--- FlaskFromYoutube/controller/user_controller.py
+++ FlaskFromYoutube/controller/user_controller.py
@@ -1,5 +1,8 @@
 from app import app
+from model.user_model import user_model
+
+u_model = user_model()
 
 @app.route('/user/signup')
 def signup():
-    return 'Signup page created'
+    return u_model.user_signup_model() + '\n signup controller operation'
